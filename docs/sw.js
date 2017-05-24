36567883a11f07efe120e0442a49eb6c0069219d
deepak-thapa <deepakthapa445.dt@gmail.com>
2017-05-24 13:41:01 +0530

    Create sw.js

---
+++ docs/sw.js
@@ -0,0 +1,14 @@
+//Service worker
+/*self.addEventListener('install',e=>{
+console.log("Installing service worker");
+e.waitUntill(
+caches.open(cacheName).
+then(cache=>{
+return cache.addAll(files);
+})
+
+.then(_=>{
+console.log("caching successfull");
+})
+.catch(err=>{
+*/
